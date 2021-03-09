5a5a377b6669e1df76a43a5c2d9dc06a1dde8e6f
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2021-03-09 22:18:31 +0100

    Setup dev

--- setup_cosmos_db.bat
+++ setup_cosmos_db.bat
@@ -1,10 +1,8 @@
 
-
-
-echo Creating CosmosDB account and db with name=%1 in resource group=%2
+echo Creating CosmosDB account %1 and db %3 in resource group=%2
 set RG_NAME=%1
 set ACC_NAME=%2
-set DB_NAME=ChildrenTodoListDb
+set DB_NAME=%3
 call az cosmosdb create --name %ACC_NAME% --resource-group %RG_NAME% --enable-free-tier
 call az cosmosdb sql database create --account-name %ACC_NAME% --resource-group %RG_NAME% --name %DB_NAME%
 
--- setup_dev.bat
+++ setup_dev.bat
@@ -1,11 +1,12 @@
 
 rem RG_NAME=children-todo-list-dev-rg 
 rem set APP_NAME=ChildrenTodoListDev
-set RG_NAME=children-todo-list-test-test-rg 
-set COSMOS_DB_ACC_NAME=child-todo-list-cosmos-db-dev 
-set APP_NAME=ChildrenTodoListTestTest
+set RG_NAME=children-todo-list-dev-rg 
+set COSMOS_DB_ACC_NAME=child-todo-list-cosmos-db-dev
+set APP_NAME=ChildrenTodoListDev
+set DB_NAME=ChildrenTodoListDb
 
 call az group create -l westeurope -n %RG_NAME%
 
-rem call setup_cosmos_db.bat %RG_NAME% %COSMOS_DB_ACC_NAME%
-call setup_rest_api.bat %RG_NAME% %COSMOS_DB_ACC_NAME% %APP_NAME%
+call setup_cosmos_db.bat %RG_NAME% %COSMOS_DB_ACC_NAME% %DB_NAME%
+call setup_rest_api.bat %RG_NAME% %COSMOS_DB_ACC_NAME% %APP_NAME% %DB_NAME%
